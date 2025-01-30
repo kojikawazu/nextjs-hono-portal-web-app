5d7bbfc5d51e803a4cf8ca8851af420af4fa4faf
kojikawazu <nanairo7.daydream21@gmail.com>
2025-01-30 21:50:21 +0900

    バグ解消

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -1,14 +1,18 @@
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
+import { PulseLoader } from 'react-spinners';
+// contexts
+import { useCommonData } from '@/app/contexts/CommonContext';
 
 /**
  * ヒーローセクション
  * @returns JSX.Element
  */
 const Hero = () => {
+    const { isLoading, commonData } = useCommonData();
+
     return (
         <div className="min-h-screen flex items-center justify-center bg-dark text-white relative overflow-hidden">
-            {/* Background gradient effects */}
             <div className="absolute inset-0">
                 <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-primary/20 rounded-full filter blur-3xl animate-fade-in-slow"></div>
                 <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-blue-500/10 rounded-full filter blur-3xl animate-fade-in-slow"></div>
@@ -31,21 +35,27 @@ const Hero = () => {
                     多様な技術を探求し、最適な価値を提供します。 
                 </p>
 
-                <div className="flex flex-col sm:flex-row gap-4 justify-center">
-                    <Link
-                        href="https://introtechkk.com"
+                {isLoading ? (
+                    <div className="h-16 flex items-center justify-center">
+                        <PulseLoader color="#ffffff" size={10} />
+                    </div>
+                ) : (
+                    <div className="flex flex-col sm:flex-row gap-4 justify-center">
+                        <Link
+                            href={commonData?.portfolioUrl || ''}
                         className="inline-flex items-center px-6 py-3 bg-primary hover:bg-primary/90 text-dark font-medium rounded-full transition-colors duration-200"
                     >
                         View Portfolio
                         <ArrowRight className="ml-2 h-4 w-4" />
-                    </Link>
-                    <Link
-                        href="/contact"
-                        className="inline-flex items-center px-6 py-3 bg-secondary hover:bg-secondary/80 text-white font-medium rounded-full transition-colors duration-200"
-                    >
-                        Contact
-                    </Link>
-                </div>
+                        </Link>
+                        <Link
+                            href="/contact/form"
+                            className="inline-flex items-center px-6 py-3 bg-secondary hover:bg-secondary/80 text-white font-medium rounded-full transition-colors duration-200"
+                        >
+                            Contact
+                        </Link>
+                    </div>
+                )}
             </div>
         </div>
     );
